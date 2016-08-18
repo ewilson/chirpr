be9bff59c2466ac7fe895943291c69cc993299dd
Eric Wilson <eric.wilson@aver.io>
2016-08-17 21:46:41 -0400

    added some chirps

--- data/test_chirpr.sql
+++ data/test_chirpr.sql
@@ -19,9 +19,18 @@ CREATE TABLE chirp (
     datetime TEXT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES user(id)
 );
+INSERT INTO "chirp" VALUES(1,'Hello?',1,'2016-08-17 21:26:33.364727');
+INSERT INTO "chirp" VALUES(2,'Is this thing on?',2,'2016-08-17 21:26:43.364727');
+INSERT INTO "chirp" VALUES(3,'I saw that!',3,'2016-08-17 21:43:33.364727');
+INSERT INTO "chirp" VALUES(4,'40 characters, limiting',1,'2016-08-17 23:26:33.364727');
+INSERT INTO "chirp" VALUES(5,'I like pie',2,'2016-08-18 21:26:33.364727');
+INSERT INTO "chirp" VALUES(6,'I like python',1,'2016-09-17 21:26:33.364727');
+INSERT INTO "chirp" VALUES(7,'The web is sublime',3,'2016-09-17 21:26:33.364727');
+INSERT INTO "chirp" VALUES(8,'Who is Tim Berners-Lee?',4,'2017-08-17 21:26:33.364727');
 CREATE TABLE migration (
     script TEXT PRIMARY KEY
 );
 DELETE FROM sqlite_sequence;
 INSERT INTO "sqlite_sequence" VALUES('user',6);
+INSERT INTO "sqlite_sequence" VALUES('chirp',8);
 COMMIT;
